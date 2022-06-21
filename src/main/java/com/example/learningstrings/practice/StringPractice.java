e26bf28a48005393e41371e20157c34448373477
Kelso Griffin <kgriffin.c@xpanxion.com>
2022-06-21 13:10:12 -0500

    Added new scenarios for substrings and outlining the other useful String methods given by Java

--- src/main/java/com/example/learningstrings/practice/StringPractice.java
+++ src/main/java/com/example/learningstrings/practice/StringPractice.java
@@ -5,24 +5,41 @@ public class StringPractice {
     public static void main(String args[]){
         String str = "Learn string handling";
         String allUpper = "LEARN STRING HANDLING";
-        boolean isTrue = false;
+        String subStr = "";
+
+        boolean isEqualsTrue = false;
+        boolean isEqualsIgnoreCaseTrue = false;
 
         //This will return the first char of the string
         char ch1 = str.charAt(0);
         char ch2 = str.charAt(1);
         char ch3 = str.charAt(4);
 
-        if(str.equalsIgnoreCase(allUpper)) {
-            isTrue = true;
+        if(str.equals(allUpper)) {
+            isEqualsTrue = true;
         }else {
-            isTrue = false;
+            isEqualsTrue = false;
         }
+
+        if(str.equalsIgnoreCase(allUpper)) {
+            isEqualsIgnoreCaseTrue = true;
+        }else {
+            isEqualsIgnoreCaseTrue = false;
+        }
+
+        subStr = str.substring(0, 6).trim();
 
         System.out.println("Character at 0 index is: " + ch1);
         System.out.println("Character at 1 index is: " + ch2);
         System.out.println("Character at 4 index is: " + ch3);
 
-        System.out.println("Are str and allUpper equal: " + isTrue);
+        System.out.println("Are str and allUpper equals- Ignore Case  : " + isEqualsIgnoreCaseTrue);
+        System.out.println("Are str and allUpper equals  : " + isEqualsTrue);
+
+        System.out.println("Original string: " + str + " - Substring :" + subStr);
+
+
+
 
         // 1) Using the string "Learn string handling": what index is the last letter in the string?
 
@@ -34,5 +51,16 @@ public class StringPractice {
 
 
         // 4) Create a scenario that returns false if the character at the 0 index equals the 5 index
+
+        //length()
+        //equals()
+        //equalsIgnoreCase()
+        //charAt()
+        //contains()
+        //substring()
+        //startsWith
+        //endsWith
+        //toLowerCase()
+        //toUpperCase()
     }
 }
